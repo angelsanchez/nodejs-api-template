df1a4a4d1e9ced87059574e8562a9604dd8c7443
Angel Sanchez <angel.sanchez@intelygenz.com>
2015-01-13 18:24:20 +0100

    App code refactor to import routes, renamed folder routers to routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 var log = require('./util/log'),
-	restify = require('restify'),
-	path = require('path'),
-	config = require('config'),
-	fs = require('fs'),
-	routers_path = path.resolve(process.cwd(), 'src/routers');
+  restify = require('restify'),
+  path = require('path'),
+  config = require('config'),
+  fs = require('fs'),
+  ROUTES_FOLDER = './routes/',
+  server;
 
 //
 // App
 //
-var server = restify.createServer({
-	name : config.get('app.name'),
-	version : config.get('app.version'),
-	log: log
+server = restify.createServer({
+  name: config.get('app.name'),
+  version: config.get('app.version'),
+  log: log
 });
 
 server.use(restify.requestLogger());
-server.on('after', function (request, response, route, error) {
-	request.log.info({ req: request, res: response, error: error }, 'Request');
+server.on('after', function(request, response, route, error) {
+  request.log.info({req: request, res: response, error: error}, 'Request');
 });
 
 server.use(restify.queryParser());
@@ -26,17 +27,16 @@ server.use(restify.fullResponse()); // Sets up all of the default headers
 //
 // Routers
 //
-fs.readdirSync(routers_path).forEach(function (file) {
-	if (~file.indexOf('.js')) {
-		require(path.resolve(routers_path, file))(server);
-		log.info('Router ' + file + ' loaded');
-	}
+fs.readdirSync(path.join(__dirname, ROUTES_FOLDER)).forEach(function loadRoutes(file) {
+  if (~file.indexOf('.js')) {
+    require(ROUTES_FOLDER + file)(server);
+    log.info('Router ' + file + ' loaded');
+  }
 });
 
-
 server.use(restify.serveStatic({
-	directory: path.join(__dirname, 'public'),
-	default: 'index.html'
+  directory: path.join(__dirname, 'public'),
+  default: 'index.html'
 }));
 
 module.exports = server;
